3510b8030db5c9af4013ae860992d45365e27f16
Pavalakumaran Vithurshan <pavalakumaran.21@cse.mrt.ac.lk>
2024-10-08 23:19:51 +0530

    ..

--- db.js
+++ db.js
@@ -1,12 +1,8 @@
-const Pool = require("pg").Pool;
+const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: "postgres",
-  password: process.env.DB_PASSWORD,
-  host: "localhost",
-  port: 5432,
-  database: "AgroSL",
+  connectionString: process.env.POSTGRES_URL,
 });
 
 module.exports = pool;
