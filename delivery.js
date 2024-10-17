b670f31606ee51797864ae7949f6c77dae404c32
udesh p.v.d <damitha.21@cse.mrt.ac.lk>
2024-10-17 09:18:52 +0530

    change in order tracking

--- delivery.js
+++ delivery.js
@@ -106,12 +106,7 @@ router.get("/delivery-by-orderID/:orderID", async (req, res) => {
       "SELECT * FROM delivery WHERE order_id = $1",
       [orderID]
     );
-
-    if (result.rows.length === 0) {
-      return res.status(404).send("No delivery found for this order");
-    } else {
-      return res.status(200).json(result.rows[0]);
-    }
+    res.status(200).json(result.rows);
   } catch (e) {
     console.error(e);
     return res.status(500).send("Server error");
